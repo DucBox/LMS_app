65eb927ce1f1b19502ca7ea59231d1b71b382936
Ngo Quang Duc <ngoquangduc@MacBook-Air-cua-Hoang.local>
2024-11-19 11:40:58 +0700

    Update email configuration in settings.py

--- config/settings.py
+++ config/settings.py
@@ -193,11 +193,13 @@ EMAIL_BACKEND = config(
     "EMAIL_BACKEND", default="django.core.mail.backends.smtp.EmailBackend"
 )
 EMAIL_HOST = config("EMAIL_HOST", default="smtp.gmail.com")
-EMAIL_PORT = config("EMAIL_PORT", default=587)
+EMAIL_PORT = config("EMAIL_PORT", default=587, cast=int)
 EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=True, cast=bool)
-EMAIL_HOST_USER = config("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
-EMAIL_FROM_ADDRESS = config("EMAIL_FROM_ADDRESS")
+EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="quangducngo0811@gmail.com")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="quangduc08")
+EMAIL_FROM_ADDRESS = config(
+    "EMAIL_FROM_ADDRESS", default="SkyLearn <quangducngo0811@gmail.com>"
+)
 EMAIL_USE_SSL = False
 
 # crispy config
